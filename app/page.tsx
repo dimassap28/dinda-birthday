56948f6d057d95f8156674daca68767b77170035
dimassap99 <dimas@emp.blankontech.com>
2024-02-10 03:19:22 +0700

    refactor: change scroll section to text scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,23 @@
 import Hero from "@/components/Hero/hero";
 import Photos from "@/components/Photos-scroll-section/photos";
-import ScrollSection from "@/components/Scroll-section/scroll-section";
 import Picture from "@/components/Big-picture/picture";
 import HorPhotos from "@/components/Hor-photos/horphotos";
 import Content from "@/components/Content/content";
 import Greetings from "@/components/Greetings/greetings";
 import FlowerServer from "@/components/Flower-footer/flowerserver";
+import Text from "@/components/Scroll-section/txt";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <ScrollSection />
+      <Text />
       <Photos />
       <Picture />
       <HorPhotos />
       <Content />
       <Greetings />
       <FlowerServer />
-
 
     </main>
   );
