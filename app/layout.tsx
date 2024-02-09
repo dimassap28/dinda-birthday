4e1d753e1f82fe617744ee8368930ded18a8c5c3
dimassap99 <dimas@emp.blankontech.com>
2024-02-10 03:17:34 +0700

    add: sacramento font and id script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import LocalFont from "next/font/local";
-import { Poppins } from "next/font/google";
+import { Poppins, Sacramento } from "next/font/google";
 import "./globals.css";
 import "../global.css";
 import Script from "next/script";
+import AOSinit from "@/components/AOS/AOSFunctioin";
 
 export const metadata: Metadata = {
   title: "Dinda Birthday",
@@ -14,6 +15,12 @@ const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
   variable: '--font-poppins',
+});
+
+const sacramento = Sacramento({
+  weight: ['400'],
+  subsets: ['latin'],
+  variable: "--font-sacramento",
 });
 
 const calSans = LocalFont({
@@ -27,14 +34,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${poppins.variable} ${calSans.variable} scroll-smooth antialiased`}>
+    <html lang="en" className={`${poppins.variable} ${calSans.variable} ${sacramento.variable} scroll-smooth antialiased`}>
+      <AOSinit />
       <body className="flex flex-col justify-between min-h-screen"
         data-aos-easing="ease-out-back"
         data-aos-duration="1000"
         data-aos-delay="0"
       >
         {children}
-        <Script src="https://unpkg.com/aos@2.3.1/dist/aos.js" />
+        <Script id="aos-js" src="https://unpkg.com/aos@2.3.1/dist/aos.js" />
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
       </body>
     </html>
