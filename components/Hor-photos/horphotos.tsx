67d9259751ccb9c2ea5f6b2177fae288616654cc
dimassap99 <dimas@emp.blankontech.com>
2024-02-10 03:21:41 +0700

    add: static image

--- components/Hor-photos/horphotos.tsx
+++ components/Hor-photos/horphotos.tsx
@@ -3,8 +3,8 @@ import { ClientPhotos } from "./photosFunction";
 export default function HorPhotos() {
 
     return (
-        <section className="bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-zinc-900">
-            <div className="mt-16 sm:mt-20">
+        <section className="my-60 bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-zinc-900">
+            <div className="">
                 <ClientPhotos />
             </div>
         </section>
--- components/Hor-photos/photosFunction.tsx
+++ components/Hor-photos/photosFunction.tsx
@@ -2,20 +2,15 @@
 
 import Image from "next/image"
 import image1 from '@/images/photos/image-1.jpg'
-import image2 from '@/images/photos/image-2.jpg'
-import image3 from '@/images/photos/image-3.jpg'
-import image4 from '@/images/photos/image-4.jpg'
-import image5 from '@/images/photos/image-5.jpg'
-import { animate, motion, stagger } from "framer-motion"
-import { useEffect } from "react"
+import image2 from '@/images/photos/image-4.jpeg'
+import image3 from '@/images/photos/image-2.jpeg'
+import image4 from '@/images/photos/image-3.jpg'
+import image5 from '@/images/photos/image-5.jpeg'
+import { motion } from "framer-motion"
 
 export function ClientPhotos() {
     let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
-    // useEffect(() => {
-    //     animate("image1", {
-    //         rotate: 80
-    //     })
-    // }), []
+    const fade = ['zoom-in-right', 'fade-down', 'fade-up', 'fade-down', 'zoom-in-left']
     return (
         <motion.div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8"
             initial={{ opacity: 0, x: 100 }}
@@ -26,6 +21,8 @@ export function ClientPhotos() {
                 <div
                     key={image.src}
                     className={`relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 sm:w-72 sm:rounded-2xl dark:bg-zinc-800 ${rotations[imageIndex % rotations.length]}`}
+                    data-aos={fade[imageIndex]}
+                    data-aos-delay={imageIndex * 300}
                 >
                     <Image
                         src={image}
