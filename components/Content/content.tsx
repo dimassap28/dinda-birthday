109b9abc9c7e0d0b07f0f640da73740173996b57
dimassap99 <dimas@emp.blankontech.com>
2024-02-10 03:20:32 +0700

    refactor: text and image

--- components/Content/content.tsx
+++ components/Content/content.tsx
@@ -1,47 +1,69 @@
+import Image from 'next/image'
+import Image6 from '@/images/photos/image-11.jpg'
+import Image7 from '@/images/photos/image-7.jpg'
+import Image8 from '@/images/photos/image-8.jpg'
+import Image9 from '@/images/photos/image-9.jpg'
+
 export default function Content() {
     return (
         <section>
             <div className="mt-32 overflow-hidden sm:mt-40">
                 <div className="mx-auto max-w-[59rem] px-6 lg:flex lg:px-8">
                     <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
-                        <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
-                            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Our people</h2>
-                            <p className="mt-6 text-xl leading-8 text-gray-600">
-                                Quasi est quaerat. Sit molestiae et. Provident ad dolorem occaecati eos iste. Soluta rerum quidem
-                                minus ut molestiae velit error quod. Excepturi quidem expedita molestias quas.
+                        <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8"
+                            data-aos="fade-right"
+                        >
+                            <h2 className="text-4xl font-bold tracking-tight bg-gradient-to-b from-[#fafafa] to-[#fafafa]/70 bg-clip-text text-transparent sm:text-6xl">A Birthday Blessing of True Friends</h2>
+                            <p className="mt-6 text-lg leading-relaxed bg-gradient-to-b from-[#fafafa] to-[#fafafa]/70 bg-clip-text text-transparent">
+                                On this special day, as the sun bathes the world in a warm embrace, I find myself reflecting on the beautiful tapestry of your life. Today, as we celebrate your existence, I am reminded of the extraordinary blessing of true friendship that surrounds you. Your friends, like guardian stars, illuminate your path with kindness, support, and the genuine joy that only authentic connections bring.
                             </p>
-                            <p className="mt-6 text-base leading-7 text-gray-600">
-                                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
-                                amet fugiat veniam occaecat fugiat. Quasi aperiam sit non sit neque reprehenderit.
+                            <p className="mt-6 text-lg leading-relaxed bg-gradient-to-b from-[#fafafa] to-[#fafafa]/70 bg-clip-text text-transparent">
+                                You are truly fortunate to be enveloped by the company of individuals who not only share in your laughter but also stand by you in times of need. Their goodness, kindness, and uplifting spirits are a testament to the wonderful person you are. May this birthday be a celebration not just of another year but also of the remarkable friendships that grace your journey. Happy Birthday to someone cherished and surrounded by the love of exceptional friends.
                             </p>
                         </div>
                         <div className="flex flex-wrap items-start justify-end gap-6 sm:gap-8 lg:contents">
-                            <div className="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end">
-                                <img
-                                    src="https://images.unsplash.com/photo-1670272502246-768d249768ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&q=80"
+                            <div className="w-0 flex-auto lg:ml-auto lg:w-auto lg:flex-none lg:self-end"
+                                data-aos="fade-down"
+                            >
+                                <Image
+                                    src={Image6}
                                     alt=""
+                                    width={768}
+                                    height={604}
                                     className="aspect-[7/5] w-[37rem] max-w-none rounded-2xl bg-gray-50 object-cover"
                                 />
                             </div>
                             <div className="contents lg:col-span-2 lg:col-end-2 lg:ml-auto lg:flex lg:w-[37rem] lg:items-start lg:justify-end lg:gap-x-8">
-                                <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto">
-                                    <img
-                                        src="https://images.unsplash.com/photo-1605656816944-971cd5c1407f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80"
+                                <div className="order-first flex w-64 flex-none justify-end self-end lg:w-auto"
+                                    data-aos="fade-right"
+                                >
+                                    <Image
+                                        src={Image7}
                                         alt=""
+                                        width={768}
+                                        height={604}
                                         className="aspect-[4/3] w-[24rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
                                     />
                                 </div>
-                                <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none">
-                                    <img
-                                        src="https://images.unsplash.com/photo-1568992687947-868a62a9f521?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1152&h=842&q=80"
+                                <div className="flex w-96 flex-auto justify-end lg:w-auto lg:flex-none"
+                                    data-aos="fade-up"
+                                >
+                                    <Image
+                                        src={Image8}
                                         alt=""
+                                        width={2000}
+                                        height={2000}
                                         className="aspect-[7/5] w-[37rem] max-w-none flex-none rounded-2xl bg-gray-50 object-cover"
                                     />
                                 </div>
-                                <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none">
-                                    <img
-                                        src="https://images.unsplash.com/photo-1612872087720-bb876e2e67d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=768&h=604&q=80"
+                                <div className="hidden sm:block sm:w-0 sm:flex-auto lg:w-auto lg:flex-none"
+                                    data-aos="fade-left"
+                                >
+                                    <Image
+                                        src={Image9}
                                         alt=""
+                                        width={768}
+                                        height={604}
                                         className="aspect-[4/3] w-[24rem] max-w-none rounded-2xl bg-gray-50 object-cover"
                                     />
                                 </div>
