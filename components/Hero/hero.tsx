4da4a5853ba1674a9e21c522ec5e5f593f40ddc4
dimassap99 <dimas@emp.blankontech.com>
2024-02-10 03:35:38 +0700

    add: missing transition line

--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -10,10 +10,10 @@ export default function Hero() {
                     quantity={100}
                 />
 
-                <h1 className="z-10 text-4xl h-36 text-transparent text-center duration-1000 bg-gradient-to-b from-[#fafafa] to-[#4d4d4d] cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+                <h1 className="z-10 text-4xl text-transparent duration-1000 bg-gradient-to-b from-[#fafafa] to-[#4d4d4d] cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
                     Hi, Dinda!
                 </h1>
-
+                <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
             </div>
         </section>
     )
